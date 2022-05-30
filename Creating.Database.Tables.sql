70b3b0ba3fd5d9c3ff99cf4a230251dd54cd006b
ldctp <luciannadaisy@gmail.com>
2022-05-30 11:07:30 +0100

    Add Creating.Database.Tables.sql

--- Creating.Database.Tables.sql
+++ Creating.Database.Tables.sql
@@ -3,24 +3,28 @@ USE film
 
 CREATE TABLE info 
 (film_id INTEGER NOT NULL,  
- f_name TEXT NULL,    
+ f_name TEXT NOT NULL,    
  f_year INTEGER NOT NULL);
-CONSTRAINT
-<constraint_name>
-<constraint_type>
-(<column_it_applies_to>);
+PRIMARY KEY (film_id);
 
 CREATE TABLE rating
-(film_id INTEGER NOT NULL, 
+(rating_id INTEGER NOT NULL,
+film_id INTEGER NOT NULL, 
 my_rating FLOAT NOT NULL,
 imdb_rating FLOAT NOT NULL,
-rot.tom.rating FLOAT NOT NULL)
+rot.tom.rating FLOAT NOT NULL);
+PRIMARY KEY (rating_id),
+FOREIGN KEY (film_id) REFERENCES info(film_id);
 
 CREATE TABLE diversity
-(black_and_white BOOLEAN NOT NULL,
-animated BOOLEAN NOT NULL
-female_writer BOOLEAN NOT NULL,
-female_director BOOLEAN NOT NULL,
-POC_writer BOOLEAN NOT NULL,
-POC_director BOOLEAN NOT NULL
+(diversity_id INTEGER,
+film_id INTEGER, 
+black_and_white BOOLEAN,
+animated BOOLEAN,
+female_writer BOOLEAN,
+female_director BOOLEAN,
+POC_writer BOOLEAN,
+POC_director BOOLEAN);
+PRIMARY KEY (diversity_id)
+FOREIGN KEY (film_id) REFERENCES info(film_id);
 
